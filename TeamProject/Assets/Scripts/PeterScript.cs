07f32e7033fb88e051097ffe74d9d1bf9ab127b5
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2021-02-09 00:12:14 -0500

    Test

--- TeamProject/Assets/Scripts/PeterScript.cs
+++ TeamProject/Assets/Scripts/PeterScript.cs
@@ -7,8 +7,6 @@ using UnityEngine.SceneManagement;
 public class PeterScript : MonoBehaviour
 {
     public AudioSource BackgroundMusic;
-    public AudioSource VictoryMusic;
-    public AudioSource DefeatMusic;
     public AudioClip gemSound;
     public AudioClip shieldSound;
     public AudioClip killSound;
@@ -59,8 +57,6 @@ public class PeterScript : MonoBehaviour
         BackgroundMusic.loop = true;
         winText.text = "";
         loseText.text = "";
-        VictoryMusic.enabled = false;
-        DefeatMusic.enabled = false;
     }
     void Flip()
     {
@@ -153,8 +149,7 @@ public class PeterScript : MonoBehaviour
         {
             BackgroundMusic.enabled = false;
             speed = 0.0f;
-            loseText.text = "Game Over You Lose! Press R to go to the Menu Screen or ESC to quit.";
-            DefeatMusic.enabled = true;
+            loseText.text = "Game Over You Lose! Press R to go to the Menu Screen";
             if (Input.GetKey(KeyCode.R))
             {
                 SceneManager.LoadScene("Menu");
@@ -176,10 +171,9 @@ public class PeterScript : MonoBehaviour
         {
             if (gemValue == 89)
             {
-                winText.text = "Congratulations You Win! Press R to go to the Menu Screen or ESC to quit.";
+                winText.text = "Congratulations You Win! Press R to go to the Menu Screen";
                 BackgroundMusic.enabled = false;
                 speed = 0.0f;
-                VictoryMusic.enabled = true;
                 if (Input.GetKey(KeyCode.R))
                 {
                     SceneManager.LoadScene("Menu");
